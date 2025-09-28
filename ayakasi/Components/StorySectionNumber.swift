237b2d779683d1cb6f9bb6c76d4261b54b12c611
phihash <insharp0220@gmail.com>
2025-09-28 16:51:29 +0900

    モデルと画像更新

---
+++ ayakasi/Components/StorySectionNumber.swift
@@ -0,0 +1,18 @@
+//
+//  StorySectionNumber.swift
+//  ayakasi
+//
+//  Created by Kento Nakai on 2025/09/28.
+//
+
+import SwiftUI
+
+struct StorySectionNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StorySectionNumber()
+}
