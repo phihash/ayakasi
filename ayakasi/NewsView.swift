2b3cdf170356c341f922602d35c11ee2e04f28a1
phihash <insharp0220@gmail.com>
2025-10-07 00:10:09 +0900

    データ修正

---
+++ ayakasi/NewsView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsView.swift
+//  ayakasi
+//
+//  Created by Kento Nakai on 2025/10/05.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsView()
+}
