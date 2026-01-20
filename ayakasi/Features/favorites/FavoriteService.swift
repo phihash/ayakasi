4f718da6808d60348c4742a11eff26a91dadc73d
phihash <insharp0220@gmail.com>
2026-01-20 23:52:40 +0900

    fetch関数

--- ayakasi/Features/favorites/FavoriteService.swift
+++ ayakasi/Features/favorites/FavoriteService.swift
@@ -7,23 +7,22 @@ class FavoriteService : ObservableObject{
     static let shared = FavoriteService()
     private let db = Firestore.firestore()
     private let authService = AuthService.shared
-    @Published var bookmarkedCommentIds: Set<String> = []
+    @Published var bookmarkedCommentIds: [String] = []
     
     private init(){
         
     }
     
-    @Published var isBookmarkCommentsLoafing: Bool = false
+    @Published var isBookmarkCommentsLoading: Bool = false
     
     func fetchBookmarkComments() async throws {
         guard let userId = authService.currentUser?.uid else {return}
         
-        isBookmarkCommentsLoafing = true
-        defer {isBookmarkCommentsLoafing = false}
+        isBookmarkCommentsLoading = true
+        defer {isBookmarkCommentsLoading = false}
         
         let useRef = try await db.collection("users").document(userId).getDocument()
-        let bookmarkComments = useRef.get("bookmarkCommentIds") as? [String] ?? []
-        
+        bookmarkedCommentIds = useRef.get("bookmarkedComments") as? [String] ?? []
     }
     
     func bookmarkComments(_ commentId: String){
