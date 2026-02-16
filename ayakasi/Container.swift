93f90dff5364f77c50c95d7a86a0ceef2752d995
phihash <insharp0220@gmail.com>
2026-02-16 23:43:44 +0900

    アイコン、タブの位置変更

--- ayakasi/Container.swift
+++ ayakasi/Container.swift
@@ -4,45 +4,45 @@ struct Container: View {
     @State private var selection : Int = 0
     var body: some View {
         ZStack{
-
+            
             TabView(selection: $selection) {
-
+                
                 SearchView()
                     .tabItem {
                         Image("search")
                             .renderingMode(.template)
                     }
                     .tag(0)
-
-                HomeView()
+                
+                CommunityView()
                     .tabItem {
-                        Image("news")
+                        Image("conversation")
                             .renderingMode(.template)
                     }
                     .tag(1)
-
-
-                CommunityView()
+                
+                JapanView()
                     .tabItem {
-                        Image("comment")
+                        Image("map1")
                             .renderingMode(.template)
                     }
                     .tag(2)
-
-                JapanView()
+                
+                HomeView()
                     .tabItem {
-                        Image("locationcity")
+                        Image("event1")
                             .renderingMode(.template)
                     }
                     .tag(3)
-
+                
+                
                 SettingView()
                     .tabItem {
                         Image("setting")
                             .renderingMode(.template)
                     }
                     .tag(4)
-
+                
             }
             .tint(.blue)
         }
