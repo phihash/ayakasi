c124c6d7d9c523cafa8a55490944c03ae27f2c9d
phihash <insharp0220@gmail.com>
2026-04-06 21:48:14 +0900

    お問い合わせページ追加

--- ayakasi/MessageUI.swift
+++ ayakasi/MessageUI.swift
@@ -1,7 +1,43 @@
 import SwiftUI
 
+struct Message: Identifiable {
+    let id = UUID()
+    let content: String
+    let isHandled: Bool
+}
+
 struct MessageUI: View {
+    @State private var messages: [Message] = [
+        Message(
+            content: "新しい妖怪を追加して欲しい、1 ぬっぺっほふ 2 けうけげん 3 さんもとごぶろうざえもん",
+            isHandled: true
+        )
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text("公開していいとチェックしていただいたものを表示しています")
+                .font(.subheadline)
+                .padding(.vertical,8)
+            List(messages) { message in
+                VStack(alignment: .leading, spacing: 8) {
+                    HStack {
+
+                        Spacer()
+
+                        if message.isHandled {
+                            Image(systemName: "checkmark")
+                        }
+                    }
+
+                    Text(message.content)
+                        .font(.body)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                }
+            }
+            .navigationTitle("お問い合わせ一覧")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
